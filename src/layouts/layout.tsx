1d02ae42b024c22f37e3fd5db14570575328daab
DionneChelini <purplestreetsmarts@gmail.com>
2023-01-16 06:27:54 -0800

    second commit

---
+++ src/layouts/layout.tsx
@@ -0,0 +1,17 @@
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
+import { ReactNode } from 'react'
+
+interface propTypes {
+    children: ReactNode
+}
+
+export default function Layout({ children }: {children: propTypes['children']}) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}

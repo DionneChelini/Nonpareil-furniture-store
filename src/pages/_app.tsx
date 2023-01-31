7eacdd5681402398542fa277d49439c407f3f66f
DionneChelini <purplestreetsmarts@gmail.com>
2023-01-30 23:41:20 -0800

    types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
-export default function App({ Component, pageProps }: AppProps) {
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
 
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
 
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
 
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(<Layout><Component {...pageProps} /></Layout>)
 }

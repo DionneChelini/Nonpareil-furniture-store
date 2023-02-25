e6b5c388fe9eaf08954bb1c78602107bc2c4114b
DionneChelini <purplestreetsmarts@gmail.com>
2023-02-24 22:59:19 -0800

    removed blog

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'strapi342134.s3.amazonaws.com',
-                port: '1337',
+                port: '',
                 pathname: '/small_avatar_ef077e4fe6.png',
             },
         ],

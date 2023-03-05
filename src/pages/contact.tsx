3d288f66fc8ffd5263c83390b2ce935adda17ce9
DionneChelini <purplestreetsmarts@gmail.com>
2023-03-05 02:34:27 -0800

    added address

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -65,6 +65,28 @@ export default function Example() {
             </div>
 
           </div>
+          <div className="lg:grid lg:grid-cols-3 lg:gap-8 mt-24">
+            <h2 className="text-2xl font-extrabold text-gray-900 sm:text-3xl">Locations</h2>
+            <div className="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:mt-0 lg:col-span-2">
+              <div>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">Australia</h3>
+                <dl className="mt-2 text-base text-gray-500">
+                  <div>
+                    <dt className="sr-only">Melbourne, Victoria</dt>
+                    <dd>100 Harbour Esplanade, Docklands</dd>
+                    <dd>Victoria, Australia, 3008</dd>
+                  </div>
+
+                </dl>
+              </div>
+
+
+
+
+            </div>
+          </div>
+
+
         </div>
       </div>
 
